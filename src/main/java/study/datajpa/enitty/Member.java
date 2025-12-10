c25a87de21d08a44f1a50a0ec934d7b55782e9a0
jerry8534 <jerryjjinu@gmail.com>
2025-12-10 15:05:17 +0900

    Section 3

--- src/main/java/study/datajpa/enitty/Member.java
+++ src/main/java/study/datajpa/enitty/Member.java
@@ -1,24 +1,38 @@
 package study.datajpa.enitty;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"io", "usename", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id", nullable = false, updatable = false)
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username){
         this.username = username;
+    }
+
+    public Member(String member1, int i, Team team) {
+        this.username = member1;
+        this.age = i;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
